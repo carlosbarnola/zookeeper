ece5b8b55bb09b18153f09af31071831ab66a816
Carlos Rojas <carlosrb206@gmail.com>
2021-11-16 17:59:01 -0500

    zookeper code

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalsRoutes');
+
+router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
+
+module.exports = router;
